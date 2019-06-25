8e7489be88a8b0b1d979ae56c82baa340e5500a3
fernando43 <fernando@sigalei.com.br>
2019-06-25 09:18:17 -0300

    isort

--- test_file.py
+++ test_file.py
@@ -1,16 +1,20 @@
 import re
 
-import bs4
+from pre_commit_test.local_lib import LocalLibClass
 
-from local_lib import LocalLibClass
-
-global_variable = 10
+GLOBAL_VARIABLE = 10
 
 
+# pylint: disable=too-few-public-methods
 class TestClass(object):
     def __init__(self):
         self.first_variable = 1
         self.second_variable = [
             value for value in range(10, 1000, 2) if value % 10 == 3
         ]
-        self.third_variable = 'This is a very very very long string that shouldn\'t be in one line only because it has more than 80 characters'
+        self.third_variable = (
+            'This is a very very very long string that shouldn\'t be in one '
+            'line only because it has more than 80 characters')
+
+        self.local_lib_reference = LocalLibClass()
+        self.regex = re.compile(r'\d+')
